2115439c396462fd93540ca4e6bcb54e8085765f
Nikhil Prasanna Raman <nraman56@uw.edu>
2019-01-15 20:22:35 -0800

    started exercise, got through parts 1 and 2, in part 3

--- week-2/exercise.R
+++ week-2/exercise.R
@@ -1,7 +1,7 @@
 ## Part 1: Debugging
 
-my_num <- 6
-initials <- "?. ?."
+my_num <- 5
+initials <- "N. R."
 
 my_vector <- c(my_num, initials)
 
@@ -9,7 +9,11 @@ my_vector <- c(my_num, initials)
 vector_sum <- sum(my_vector)
 
 # Describe why this doesn't work: 
-# 
+# Response: This doesn't work because the two data types present in my_vector,
+# type String and what I assume is type Integer, are not being concatenated rather
+# than being plainly added together, which is not possible; something with a concrete value
+# cannot be added to by something without concrete value on principle, so why should an Integer
+# be able to be added to by a String?
 
 install.packages("stringr")
 
@@ -18,20 +22,27 @@ my_line <- "Hey, hey, this is the library"
 print(str_length(my_line))
 
 # Describe why this doesn't work: 
-# 
+# Response: This doesn't work because there is no call to the library for stringr, I.e. library(stringr)
 
 said_the_famous <- paste(my_line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# Response: 1. the variable "initial" was never assigned a value
+#           2. the separating/collapsing argument, in this case " - ", needs to come 
+#           after all of the other input arguments, like my_line and initial (had it been instantiated)
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
+test_vector <- c(1 > 2, nchar("Contradiction") < nchar("Truth"))
+# Response: typeof(test_vector) returns type "logical"
 
 # Write a function `compare_length` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
+compare_length <- function(vector_1, vector_2) {
+  return <- "The difference in length is N"
+}
 
 
 # Pass two vectors of different length to your `compare_length` function
