a51d9263dbac6a7958f7b1f54838f3814542fc28
Nikhil Prasanna Raman <nraman56@uw.edu>
2019-01-24 20:30:38 -0800

    finished week 3 exercises, solidified absolute basics of data frames

--- week-3/week-3.R
+++ week-3/week-3.R
@@ -2,14 +2,19 @@
 
 # What could be an issue here?
 event_capacity <- c(60, 50, '100', 100)
+# I want to say that '100' is a character, while the rest of the values are
+# numerics, converting everything into character/string values
 
 # Fix the possible error in a new variable event_capacity_new
+event_capacity_new <- c(60, 50, 100, 100)
 
 # Create a vector of event names for each event 
-event_names <- 
+event_names <- c("wedding", "bar crawl", "sportsball game", "genetic experimentation")
   
 # Fix the error in creating this dataframe
-events <- data.frame(event_names, event_cost, event_capacity)
+events <- data.frame(event_names,  event_capacity, stringsAsFactors = FALSE)
+# needs to make stringsAsFactors false to properly work. Also, there is no event_costs
+# vector, and the event_capacity vector should be the new one
 
 # Check the event_capacity column
 
@@ -20,12 +25,14 @@ events <- data.frame(event_names, event_capacity_new)
 
 # Are there any differences between event_capacity and event_capacity)_new?
 # Answer here
+# the new one provides numeric values, while the initial one provides string values
 
 # Fix an error of adding columns to the dataframe
-events.event_attendance <- c(54, 43, 85, 90)
+events$event_attendance <- c(54, 43, 85, 90)
+# should be a $ instead of a .
 
 # Add a column called seats_left in the events dataframe
-events$seats_left <-
+events$seats_left <- c(event_capacity_new - events$event_attendance)
   
   
   
